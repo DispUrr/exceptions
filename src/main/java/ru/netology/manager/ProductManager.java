c6db554c89c1c69d53f9269bdbfa7fce55dbdedf
Maria Sedova <mariassedova@mail.ru>
2021-05-15 15:15:20 +0300

    project added

---
+++ src/main/java/ru/netology/manager/ProductManager.java
@@ -0,0 +1,20 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Product;
+import ru.netology.repository.ProductRepository;
+
+public class ProductManager {
+    private final ProductRepository repository;
+
+    public ProductManager(ProductRepository repository) {
+        this.repository = repository;
+    }
+
+    public void add(Product product) {
+        repository.save(product);
+    }
+
+    public void removeById(int id) {
+        repository.removeById(id);
+    }
+}
